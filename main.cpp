b423e6f6000bc4b41c021ef6b379c661ba6fe7be
Graham Taylor <aelfgard@gmail.com>
2017-09-21 23:06:56 -0500

    manual

---
+++ main.cpp
@@ -0,0 +1,75 @@
+#include <string>
+#include <iostream>
+#include <vector>
+
+struct MadLibChunk
+{	
+	std::string before;
+	std::string prompt;
+	std::string after;	
+	std::string userInput;
+
+	void GetUserInput()
+	{
+		std::cout << prompt;
+		std::getline(std::cin, userInput);		
+	}
+
+	void Display() const
+	{
+		std::cout << before << userInput << after;
+	}
+};
+
+std::vector<MadLibChunk> parse(const std::string& str)
+{
+	std::vector<MadLibChunk> chunklist;
+	int iAt = 0;
+	while (iAt < str.length() && iAt >= 0)
+	{
+		MadLibChunk chunk;
+
+		int next = str.find('{', iAt);		
+		chunk.before = str.substr(iAt, next-iAt);
+
+		if (next == -1)
+		{			
+			break;
+		}
+
+		int close = str.find('}', next);
+		chunk.prompt = str.substr(next + 1, close - next-1) + ": ";	
+
+		chunklist.push_back(chunk);
+		iAt = close + 1;
+	}
+
+	chunklist.back().after = str.substr(iAt);
+
+	return chunklist;
+}
+
+
+int main()
+{
+	std::string str = "\
+I spent last summer on my grandfather's {Adjective} farm.  He raises {noun/s} for local food {noun/s}.  He also grows \
+corn on the {noun}, {adjective} lettuce, and lima {noun/s}.  My favorite place to {verb} on the farm is the {adjective} house where \
+grandfather keeps his {noun/s}.  But when I visit in November, there are no {noun/s}!  They are all gone!  I anxiously await at the table \
+that Thanksgiving.  I am relived when Grandma brings out the {noun} for Thanksgiving dinner.";
+
+	auto entries = parse(str);	
+
+	for (auto& e : entries)
+	{
+		e.GetUserInput();
+	}
+
+	for (const auto& e : entries)
+	{
+		e.Display();
+	}
+	
+    return 0;
+}
+
